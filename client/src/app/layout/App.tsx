29b0efbc721f5d572d419c6a7714a1b6f5dbca4a
pharret31 <pharret31@gmail.com>
2022-06-30 18:42:10 +0300

    End of section 4

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,13 +1,18 @@
 import { createTheme, CssBaseline, ThemeProvider } from '@mui/material'
 import { Container } from '@mui/system'
 import { useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import AboutPage from '../../features/about/AboutPage'
 import Catalog from '../../features/catalog/Catalog'
+import ProductDetails from '../../features/catalog/ProductDetails'
+import ContactPage from '../../features/contact/ContactPage'
+import HomePage from '../../features/home/HomePage'
 import Header from './Header'
 
 function App() {
     const [darkMode, setDarkMode] = useState(false)
     const palleteType = darkMode ? "dark" : "light"
-    
+
     const theme = createTheme({
         palette: {
             mode: palleteType,
@@ -26,7 +31,13 @@ function App() {
             <CssBaseline />
             <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
             <Container>
-                <Catalog />
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/catalog" element={<Catalog />} />
+                    <Route path="/catalog/:id" element={<ProductDetails />} />
+                    <Route path="/about" element={<AboutPage />} />
+                    <Route path="/contact" element={<ContactPage />} />
+                </Routes>
             </Container>
         </ThemeProvider>
     )
